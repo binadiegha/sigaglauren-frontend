fb07515a2919edbed3e57dc11972e305ae5778c0
Tarry <opuindy@gmail.com>
2023-07-12 19:08:49 +0100

    modified firebase

--- src/Api/firebase.js
+++ src/Api/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 // Initialize Services
 const db = getFirestore();
